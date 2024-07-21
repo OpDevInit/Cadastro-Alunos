92ae14b2f0d8fa530b27a5c8160ae5fcb721120f
Jhonata <jholzinhooh567@gmail.com>
2024-07-21 17:26:20 -0300

    "closes #5, botão funcional, CADASTRAR"

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,65 +2,99 @@
 var students = [
     {
         id: 1,
-        nome: "Pedro Antonio",
+        name: "Pedro Antonio",
         email: "pedro123@gmail.com",
-        telefone: "(11) 93729-9937",
-        curso: "Kotlin",
-        turno: "Tarde"
+        phone: "(11) 93729-9937",
+        course: 2,
+        turn1: true,
+        turn2: false,
+        turn3: false
 
     },
-    
+    {
+        id: 2,
+        name: "Ricardo",
+        email: "Ricardo123@gmail.com",
+        phone: "(11) 93729-9937",
+        course: 3,
+        turn1: false,
+        turn2: true,
+        turn3: false
+
+    }
+
+
 
 ]
+
+var courses = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "Kotlin" },
+    { id: 3, name: "SpringBoot" },
+];
+
 loadStudents();
 
 function loadStudents() {
-    for (let prod of students) {
-        addNewRow(prod);
+    for (let stud of students) {
+        addNewRow(stud);
     }
 }
 
 //Save a product
 
-function save(){
-    /*
-    var prod = {
+function save() {
+    var stud = {
         id: students.length + 1,
         name: document.getElementById("inputName").value,
         email: document.getElementById("inputEmail").value,
-        phone:document.getElementById("inputPhone").value,
-        course: document.getElementById("selectCategory").value,
-        promotion: document.getElementById("checkBoxPromotion").checked,
-        new:document.getElementById("checkBoxNewProduct").checked,
+        phone: document.getElementById("inputPhone").value,
+        course: document.getElementById("selectCourse").value,
+        turn1: document.getElementById("turn1").checked,
+        turn2: document.getElementById("turn2").checked,
+        turn3: document.getElementById("turn3").checked,
+
+
+
     };
 
+    console.log("saves");
 
     addNewRow(stud);
     students.push(stud)
-    */
+
+    document.getElementById("formStudent").reset();
 }
 
-function addNewRow(students){
+function addNewRow(stud) {
     var table = document.getElementById("studentsTable");
 
     var newRow = table.insertRow();
 
-    var idNode = document.createTextNode(students.id);
+    var idNode = document.createTextNode(stud.id);
     newRow.insertCell().appendChild(idNode);
 
-    var nomeNode =  document.createTextNode(students.nome);
+    var nomeNode = document.createTextNode(stud.name);
     newRow.insertCell().appendChild(nomeNode);
 
-    var emailNode= document.createTextNode(students.email);
+    var emailNode = document.createTextNode(stud.email);
     newRow.insertCell().appendChild(emailNode);
 
-    var telNode = document.createTextNode(students.telefone)
+    var telNode = document.createTextNode(stud.phone)
     newRow.insertCell().appendChild(telNode);
 
-    var cursoNode = document.createTextNode(students.curso);
+    var cursoNode = document.createTextNode(courses[stud.course-1].name);
     newRow.insertCell().appendChild(cursoNode);
 
-    var turnoNode = document.createTextNode(students.turno)
-    newRow.insertCell().appendChild(turnoNode);
+    var options = "";
 
+    if (stud.turn1) {
+        options = 'Manhã';
+    } if (stud.turn2) {
+        options = 'Tarde';
+    } if (stud.turn3) {
+        options = 'Noite';
+    }
+    cell = newRow.insertCell();
+    cell.innerHTML = options;
 }
